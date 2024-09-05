cd20be9388721a44d5ebe2f5ae5bbc7e31c1525f
booploops <49113086+booploops@users.noreply.github.com>
2024-09-05 00:42:03 -0700

    fix naming

--- src/plugin.config.ts
+++ src/plugin.config.ts
@@ -3,7 +3,7 @@ import { createId } from "@paralleldrive/cuid2"
 export default {
     ce_prefix: createId(),
     identifier: 'dev.booploops.centered-immersive',
-    name: 'Butterchurn Visualizer for Cider',
+    name: 'Centered Immersive Lyrics Layout',
     description: 'Adds a visualizer to the app.',
     version: '0.0.1',
     author: 'booploops',
